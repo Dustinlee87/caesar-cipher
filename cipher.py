6015ec3f8f7c99ebf5845c79659e0b111a45fdf5
Dustinlee87 <122577752+Dustinlee87@users.noreply.github.com>
2023-10-30 11:24:07 -0400

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,1 +1,13 @@
-# add your code here
+def encrypt(plain_text, shift):
+    plain_text = plain_text.lower()
+    encrypted_text = ""
+    for i in plain_text:
+        if i.islower():
+            encrypted_text += chr((ord(i) + shift - 97) % 26 + 97)
+        else:
+            encrypted_text += i
+    return encrypted_text
+
+message = "Hello, my name is Dustin"
+encrypted_message = encrypt(message, 5)
+print(encrypted_message)# add your code here
